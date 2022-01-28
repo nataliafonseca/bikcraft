de32c850c2b74438e619fd72135d0f1f64c1e54a
Natália Fonseca <nataliabf@outlook.com>
2022-01-28 10:41:12 -0300

    feat: autoselect estimate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Ativar Links do Menu
+
 const links = document.querySelectorAll(".header-menu a");
 
 function activeLink(link) {
@@ -7,3 +9,16 @@ function activeLink(link) {
 }
 
 links.forEach(activeLink);
+
+// Ativar Itens do Orçamento
+
+const params = new URLSearchParams(location.search);
+
+function activeRadio(param) {
+  const element = document.getElementById(param);
+  if (element) {
+    element.checked = true;
+  }
+}
+
+params.forEach(activeRadio);
